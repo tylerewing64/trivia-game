c9c61ad1f8042056b1fbed89a306d5e44360f8d7
tylerewing64 <76537101+tylerewing64@users.noreply.github.com>
2022-09-22 16:45:11 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+import bank from "./questionbank.json" assert { type: "json" };
+
+const quizWrapper = document.getElementById("quiz");
+const questionElem = document.querySelector("#question");
+const answerA = document.querySelector("label[for='a']");
+const answerB = document.querySelector("label[for='b']");
+const answerC = document.querySelector("label[for='c']");
+const answerD = document.querySelector("label[for='d']");
+const submitBtn = document.getElementById("submitBtn");
+const answerChoices = document.querySelectorAll("input[name='answer']");
+const scoreLabel = document.getElementById("score");
+const quizData = bank.questionBank;
+
+let questionShown = 0;
+let score = 0;
+
+function loadQuestion() {
+  resetSelection();
+  let currentQuestion = quizData[questionShown];
+
+  questionElem.innerHTML = currentQuestion.question;
+  answerA.innerHTML = currentQuestion.a;
+  answerB.innerHTML = currentQuestion.b;
+  answerC.innerHTML = currentQuestion.c;
+  answerD.innerHTML = currentQuestion.d;
+}
+
+submitBtn.addEventListener("click", () => {
+  let selectedAnswer = getSelection();
+
+  if (selectedAnswer == quizData[questionShown].correctAnswer) {
+    score++;
+  }
+
+  scoreLabel.textContent = `Score: ${score}`;
+
+  if (selectedAnswer != "") {
+    questionShown++;
+  }
+
+  if (questionShown < quizData.length) {
+    if (selectedAnswer != "") {
+      loadQuestion();
+    }
+  } else {
+    quizWrapper.innerHTML = `<h1>Quiz completed!</h1><h1>You scored ${score}/${quizData.length} points!</h1><button id="retryBtn" onclick="location.reload();">Try again?</button>`;
+  }
+});
+
+const getSelection = () => {
+  let selectedAnswer = "";
+
+  answerChoices.forEach((e) => {
+    if (e.checked) {
+      selectedAnswer = e.id;
+    }
+  });
+
+  return selectedAnswer;
+};
+
+const resetSelection = () => {
+  answerChoices.forEach((e) => {
+    e.checked = false;
+  });
+};
+
+loadQuestion();
